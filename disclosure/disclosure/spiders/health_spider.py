c0bf16890f704ad0ae5435239eee7d920d60a7d7
djon3s <hack@riseup.net>
2012-10-03 11:38:55 +1000

    fixed parse bug in health spider

--- disclosure/disclosure/spiders/health_spider.py
+++ disclosure/disclosure/spiders/health_spider.py
@@ -5,6 +5,8 @@ from disclosure.items import DisclosureItem
 
 import html2text
 
+import dateutil.parser
+
 class HealthSpider(BaseSpider):
     name = "health"
     allowed_domains = ["http://www.health.nsw.gov.au/"]
@@ -31,7 +33,23 @@ class HealthSpider(BaseSpider):
             item['department'] = unicode('Department of Health')
             item['source'] = unicode("http://www.health.nsw.gov.au/gipaa/disclosures.asp")
             item['ref_number'] = ref_num
-            item['disclosure_date'] = rows_list[i+1]
+            print "PROBLEM"
+            print rows_list[i+1]
+            rows_list[i+1] = rows_list[i+1].encode('ascii','replace')
+            #rows_list[i+1] = rows_list[i+1].replace('\xc2\xa0', ' ')
+            #rows_list[i+1] = unicode(rows_list[i+1])
+            rows_list[i+1] = rows_list[i+1].replace('?',' ')
+            rows_list[i+1] = rows_list[i+1].replace('-',' ')
+            a,b,c = unicode(rows_list[i+1]).split(' ')
+            print a, b, c
+            if b[0] =='S':
+                rows_list[i+1]= ''.join([a, ' Sep ',  c])
+            elif b[0] == 'N':
+                rows_list[i+1]= ''.join([a, ' Nov ',  c])
+            print rows_list[i+1]
+            print a ,b ,c
+            item['disclosure_date'] = dateutil.parser.parse(rows_list[i+1]).date().strftime("%Y-%m-%d %H:%M:%S")            
+#item['disclosure_date'] = rows_list[i+1]
             item['description'] = rows_list[i+2] 
             item['release_type'] = rows_list[i+3]
             item['how_to_access'] = rows_list[i+4]
